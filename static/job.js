d43b94e8dae5173da8c77952cf611cab0f0d21a2
Mark MacGillivray <mark@cottagelabs.com>
2017-02-21 14:26:29 +0000

    adding versioning to assets

--- static/job.js
+++ static/job.js
@@ -3,8 +3,6 @@
 		file: undefined,
 		filename: '',
 		results: [],
-		done: false,
-		poll: undefined,
 		hash: undefined,
     max:3000
 	};
@@ -12,12 +10,11 @@
 	if (window.location.host.indexOf('dev.openaccessbutton.org') !== -1) job.apibaseurl = 'https://dev.api.cottagelabs.com';
 
   job.review = function() {
-		if ( $('#filereview').length ) {
-			var msg = '<p style="color:black;">Thank you. Your file appears to have ';
+		if ( $('#jobinfo').length ) {
+			var msg = '<p style="color:black;">';
 			msg += job.results.length;
-			msg += ' record rows.<br>';
-			msg += 'Please submit for processing now:</p>';
-			$('#filereview').html(msg).show();
+			msg += ' rows extracted</p>';
+			$('#jobinfo').html(msg).show();
 		}
   }
   
@@ -26,8 +23,6 @@
 		if (split === undefined) split = ',';
 		if (wrap === undefined) wrap = '"';
 		var wrapreplace = new RegExp(wrap,"g");
-		// could try to look for split and wrap chars in file somehow - looking at first char is no good because systems/people sometimes only use the wraps 
-		// when they must, like when surrounding content with a comma, but do not bother at other times
 		
     job.file = job.file.replace(/\r\n/g,'\n').replace(/\n{2,}/g,'\n').replace(/\n*$/g,'');
 		
@@ -41,8 +36,8 @@
 				il = '';
 			}
 		}
-		if (lines.length > job.max) {
-			$('#errormsg').html('<p style="color:black;">Sorry, the maximum amount of rows you can submit in one file is ' + job.max + '. Please reduce the size of your file and try again.</p>').show();
+		if (!job.max || lines.length > job.max) {
+			$('#jobinfo').html('<p style="color:black;">Sorry, the maximum amount of rows you can submit in one file is ' + job.max + '. Please reduce the size of your file and try again.</p>').show();
 			job.file = undefined;
 			job.filename = '';
 		} else {
@@ -103,13 +98,13 @@
   }
 
 	job.error = function(data) {
-		$('#filesubmit').attr('value','Submit');
-		$('#filereview').html('<p style="color:black;">Sorry, there has been an error with your submission. Please try again.<br>If you continue to receive an error, please contact us@cottagelabs.com attaching a copy of your file and with the following error information:<br>' + JSON.stringify(data) + '</p>').show();
+		$('#jobsubmit').attr('value','Submit');
+		$('#jobinfo').html('<p">Sorry, there has been an error with your submission. Please try again.<br>If you continue to receive an error, please contact us@cottagelabs.com attaching a copy of your file and with the following error information:<br>' + JSON.stringify(data) + '</p>').show();
   }
 
 	job.polling = function(data) {
-		$('.uploader').hide();
-		$('#pollinfo').show();
+		$('.jobupload').hide();
+		$('#jobinfo').show();
 		var progress = !data.data || !data.data.progress ? 0 : data.data.progress;
 		var pc = (Math.floor(progress * 10))/10;
 		var status = '<p>Job ';
@@ -117,9 +112,9 @@
 		status += '</p>';
 		if (data.data && data.data.new === true) status += '<p>Your job is new, and is still being loaded into the system. For large jobs this may take a couple of minutes.</p>';
 		status += '<p>Your job is ' + pc + '% complete.</p>';
-		status += '<p><a id="downloadresults" href="' + job.apibaseurl + '/service/job/' + job.hash + '/results?format=csv&apikey=' + clogin.apikey + '" class="btn btn-default btn-block">Download your results</a></p>';
+		status += '<p><a id="jobdownload" href="' + job.apibaseurl + '/service/job/' + job.hash + '/results?format=csv&apikey=' + clogin.apikey + '" class="btn btn-default btn-block">Download your results</a></p>';
 		if (data.data.progress !== 100) setTimeout(job.poll,10000);
-		$('#pollinfo').html(status);
+		$('#jobinfo').html(status);
 	}
 	
 	job.poll = function(hash) {
@@ -138,7 +133,7 @@
 	}
 		
   job.success = function(data) {
-		$('.uploader').hide();
+		$('.jobupload').hide();
 		try {
 			window.history.pushState("", "poll", '#' + data.data.job);
 		} catch (err) {}
@@ -147,12 +142,12 @@
   }
   
 	job.submit = function(e) {
-		$('#filereview').html("").hide();
+		$('#jobinfo').html("").hide();
 		e.preventDefault();
 		if ( !job.filename ) {
-			$('#filereview').html('<p style="color:black;">You must upload a file in order to submit. Please provide more information and try again.</p>').show();
+			$('#jobinfo').html('<p">You must upload a file in order to submit. Please provide more information and try again.</p>').show();
 		} else {
-			$('#filesubmit').attr('value','Submitting...');
+			$('#jobsubmit').attr('value','Submitting...');
 			var payload = {list:job.results,name:job.filename};
 			$.ajax({
 				url: job.apibaseurl + '/service/job?apikey='+clogin.apikey,
@@ -167,11 +162,11 @@
   }
 	  
 	job.startup = function() {
-	  $('#fileupload').on('change', job.prep);
-	  $('#filesubmit').bind('click',job.submit);
+	  $('#jobupload').on('change', job.prep);
+	  $('#jobsubmit').bind('click',job.submit);
 		if (window.location.hash && window.location.hash.replace('#','').length === 17) { // our job IDs are 17 digits long
-			setTimeout(function() {$('.uploader').hide();},200);
-			$('#pollinfo').html('<p>One moment please, retrieving job status...</p>');
+			setTimeout(function() {$('.jobupload').hide();},200);
+			$('#jobinfo').html('<p>One moment please, retrieving job status...</p>');
 			job.hash = window.location.hash.replace('#','');
 			job.poll(job.hash);
 		}
