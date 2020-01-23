6bacd0d28f87b879aedd186ddc4e7368e4151603
Joe McArthur <joseph.mcarthur.10@gmail.com>
2020-01-23 15:07:44 +0000

    add space

--- static/shareyourpaper.js
+++ static/shareyourpaper.js
@@ -440,7 +440,7 @@ var _run = function() {
       needmore = false;
       info += '<div>';
       info += '<h2>Your paper is already freely available!</h2>';
-      info += '<p>Great news, you\’re already getting the benefits of sharing your work! Your publisher or co-author have already shared it at this';
+      info += '<p>Great news, you\’re already getting the benefits of sharing your work! Your publisher or co-author have already shared it at this ';
       info += '<a target="_blank" href="' + avail.data.availability[0].url + '">freely available link</a></p>';
       info += '<h3>Give us your email to confirm deposit</h3>';
       info += '<p><input class="oabutton_form' + (_oab_opts.bootstrap !== false ? ' form-control' : '') + '" type="text" id="oabutton_email" placeholder="email@montana.edu" aria-label="email@montana.edu" style="box-shadow:none;"></input></p>';
