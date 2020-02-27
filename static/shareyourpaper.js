34907d8e3bb521c3202c0663533c8f10de800136
Joe McArthur <joseph.mcarthur.10@gmail.com>
2020-02-27 14:35:23 +0000

    add do another

--- static/shareyourpaper.js
+++ static/shareyourpaper.js
@@ -530,6 +530,7 @@ var _run = function() {
           // nothing to show, the user cannot redeposit (this is stupidly backwards to the value of the named key)
           info += '<p>Great news, you\’re already getting the benefits of sharing your work! Your publisher or co-author have already shared it.</p>';
           info += '<p><a target="_blank" href="' + avail.data.availability[0].url + '" class="btn btn-primary" style="min-width:150px;">See free version</a></p>';
+          info += '<p><b><u><a href="#" class="oabutton_restart>Do another</a></u></b></p>';
         } else {
           info += '<p>Great news, you\’re already getting the benefits of sharing your work! Your publisher or co-author have already shared it ';
           info += '<a target="_blank" href="' + avail.data.availability[0].url + '">freely available link</a>.</p>';
