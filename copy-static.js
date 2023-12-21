e02df1d4ec361b2696c031d9dabed3832273bb5e
Sophy <sssoz@users.noreply.github.com>
2023-12-21 14:26:09 +0000

    Update copy script

--- copy-static.js
+++ copy-static.js
@@ -3,20 +3,28 @@ const fs = require('fs');
 const path = require('path');
 
 function copyDir(src, dest) {
-    if (!fs.existsSync(src)) {
-        console.warn(`Source path does not exist: ${src}`);
-        return;
-    }
+  if (!fs.existsSync(src)) {
+      console.warn(`Source path does not exist: ${src}`);
+      return; // Skip if the source path doesn't exist
+  }
 
-    fs.mkdirSync(dest, { recursive: true });
-    let entries = fs.readdirSync(src, { withFileTypes: true });
+  fs.mkdirSync(dest, { recursive: true });
+  let entries = fs.readdirSync(src, { withFileTypes: true });
 
-    for (let entry of entries) {
-        let srcPath = path.join(src, entry.name);
-        let destPath = path.join(dest, entry.name);
+  for (let entry of entries) {
+      let srcPath = path.join(src, entry.name);
+      let destPath = path.join(dest, entry.name);
 
-        entry.isDirectory() ? copyDir(srcPath, destPath) : fs.copyFileSync(srcPath, destPath);
-    }
+      if (entry.isDirectory()) {
+          copyDir(srcPath, destPath);
+      } else {
+          if (fs.existsSync(srcPath)) {
+              fs.copyFileSync(srcPath, destPath);
+          } else {
+              console.warn(`File does not exist: ${srcPath}`);
+          }
+      }
+  }
 }
 
 copyDir('./static', './serve/static');
