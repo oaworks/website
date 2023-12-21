a022b99af608723ec326889fbb1a38971c2a15ef
Sophy <sssoz@users.noreply.github.com>
2023-12-21 14:21:20 +0000

    add separate script to copy static files

---
+++ copy-static.js
@@ -0,0 +1,22 @@
+// copy-static.js
+const fs = require('fs');
+const path = require('path');
+
+function copyDir(src, dest) {
+    if (!fs.existsSync(src)) {
+        console.warn(`Source path does not exist: ${src}`);
+        return;
+    }
+
+    fs.mkdirSync(dest, { recursive: true });
+    let entries = fs.readdirSync(src, { withFileTypes: true });
+
+    for (let entry of entries) {
+        let srcPath = path.join(src, entry.name);
+        let destPath = path.join(dest, entry.name);
+
+        entry.isDirectory() ? copyDir(srcPath, destPath) : fs.copyFileSync(srcPath, destPath);
+    }
+}
+
+copyDir('./static', './serve/static');
